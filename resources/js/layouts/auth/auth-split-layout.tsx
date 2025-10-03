ebd0438c3423f7d3f0b3c73bb693df1f7838cad6
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-10-03 19:05:41 +0700

    Update auth-split-layout.tsx

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -1,6 +1,6 @@
 import AppLogoIcon from '@/components/app-logo-icon';
 import { type SharedData } from '@/types';
-import { Link, usePage } from '@inertiajs/react';
+import { usePage } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
 interface AuthLayoutProps {
@@ -15,13 +15,10 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
-                <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon 
-                        className="mr-2 size-8 fill-current text-white" 
-                        src={eventSettings.event_logo || '/logo.svg'}
-                    />
-                    {eventSettings.event_name || 'Event Ticket'}
-                </Link>
+
+                <AppLogoIcon className="mr-2 size-8 fill-current text-white" src={eventSettings.event_logo || '/logo.svg'} />
+                {eventSettings.event_name || 'Event Ticket'}
+
                 {quote && (
                     <div className="relative z-20 mt-auto">
                         <blockquote className="space-y-2">
@@ -33,21 +30,11 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
             </div>
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                    <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
-                        <AppLogoIcon 
-                            className="h-10 fill-current text-black sm:h-12" 
-                            src={eventSettings.event_logo || '/logo.svg'}
-                        />
-                    </Link>
-                    <div className='flex items-center justify-center'>
-                        <AppLogoIcon 
-                            className="mr-2 size-12 fill-current text-white" 
-                            src={eventSettings.event_logo || '/logo.svg'}
-                        />
+
+                    <div className="flex items-center justify-center">
+                        <AppLogoIcon className="mr-2 size-12 fill-current text-white" src={eventSettings.event_logo || '/logo.svg'} />
                     </div>
-                    <h1 className="text-xl font-semibold text-center text-black dark:text-white">
-                        {"Silahkan masuk ke akun Anda"}
-                    </h1>
+                    <h1 className="text-center text-xl font-semibold text-black dark:text-white">{'Silahkan masuk ke akun Anda'}</h1>
                     {children}
                 </div>
             </div>
